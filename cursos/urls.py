6a1386cc92ffe45caf119169a55977d8fb33370c
BrunaCardoso7 <obitadrawing@gmail.com>
2024-11-04 23:59:35 -0300

    feat: add subcription genericsViews

--- cursos/urls.py
+++ cursos/urls.py
@@ -3,7 +3,9 @@ from .views import CursosApiView, CursoApiView, AvaliacoesApiView, AvaliacaoApiV
 
 urlpatterns = [
     path('cursos/', CursosApiView.as_view(), name='cursos'),
-    path('cursos/<int:pk>', CursoApiView.as_view(), name='curso'),
+    path('cursos/<int:curso_pk>/', CursoApiView.as_view(), name='curso'),
+    path('cursos/<int:curso_pk>/avaliacoes/', AvaliacoesApiView.as_view(), name='curso_avalicoes'),
+    path('cursos/<int:curso_pk>/avaliacoes/<int:avaliacao_pk>/', AvaliacoesApiView.as_view(), name='curso_avalicoes'),
     path('avaliacoes/', AvaliacoesApiView.as_view(), name='avaliacoes'),
-    path('avaliacoes/<int:pk>', AvaliacaoApiView.as_view(), name='avaliacao')
+    path('avaliacoes/<int:avaliacao_pk>/', AvaliacaoApiView.as_view(), name='avaliacao')
 ]
--- cursos/views.py
+++ cursos/views.py
@@ -1,6 +1,9 @@
 from rest_framework import generics
 from .models import Curso, Avaliacao
 from .serializers import AvaliacaoSerializer, CursoSerializer
+from rest_framework.generics import get_object_or_404
+from rest_framework import viewsets, mixins
+from rest_framework.decorators import action
 
 class CursosApiView(generics.ListCreateAPIView):
     queryset = Curso.objects.all()
@@ -13,8 +16,19 @@ class CursoApiView(generics.RetrieveUpdateDestroyAPIView):
 class AvaliacoesApiView(generics.ListCreateAPIView):
     queryset = Avaliacao.objects.all()
     serializer_class = AvaliacaoSerializer
+    
+    def get_queryset(self):
+        if self.kwargs.get('curso_pk'):
+            return self.queryset.filter(curso_id = self.kwargs.get('curso_pk'))
+        return self.queryset.all()
 
     
 class AvaliacaoApiView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Avaliacao.objects.all()
     serializer_class = AvaliacaoSerializer
+    
+    def get_queryset(self):
+        if self.kwargs.get('curso_pk'):
+            return get_object_or_404(self.get_queryset(), curso_id=self.kwargs.get('curso_pk'), avaliacao_pk=self.kwargs.get('avaliacao_pk'))
+        return get_object_or_404(self.get_queryset(), pk=self.kwargs.get('avaliacao_pk'))
+    
